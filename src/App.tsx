15d296d4dc4b677de41038b9bee4de4375be5c8d
Alex Burner <alex.burner@gmail.com>
2023-11-25 13:00:54 -0800

    add prettier, begin app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
 export function App() {
   return (
-      <div>Hello</div>
+    <div
+      style={{
+        position: "relative",
+        width: "1000px",
+        height: "1400px",
+        margin: "auto",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        Hello
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        Howdy
+      </div>
+    </div>
   )
 }
-
