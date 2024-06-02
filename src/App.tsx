04500d3ace38dfcc318a8399f1664a265ec87f8a
Alex Burner <alex.burner@gmail.com>
2024-06-02 16:18:11 -0700

    iterate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 
-const hcList = new Array<string>(9).fill("")
+const hcList = new Array<string>(10).fill("")
 const dcList = new Array<string>(5).fill("")
 
 export function App() {
@@ -10,9 +10,19 @@ export function App() {
         position: "relative",
         width: "1000px",
         height: "1400px",
-        margin: "auto",
+        margin: " 3% auto",
       }}
     >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        <HumanCosmology list={hcList} height={400} />
+      </div>
       <div
         style={{
           position: "absolute",
@@ -24,15 +34,52 @@ export function App() {
       >
         <DimensionComplexity list={dcList} height={1000} />
       </div>
+    </div>
+  )
+}
+
+const HumanCosmology: FC<{
+  list: unknown[]
+  index?: number
+  height: number
+}> = ({ list, index = 0, height }) => {
+  // const self = list.at(-1)
+  // const radius = 60 + 20 * (list.length - index - 1)
+  const hue = (index / (list.length - 1)) * 0.7
+  const impact = (list.length - index) / list.length
+  return (
+    <div
+      style={{
+        padding: "10px 10px",
+        margin: "0 0 10px",
+        // borderRadius: "3px",
+        borderRadius: `${impact * 2 + 3}px`,
+        // borderBottomLeftRadius: radius,
+        // borderBottomRightRadius: radius,
+        background: `hsl(${hue}turn 60% 60%)`,
+        border: `1px dotted #333`,
+        // border: `2px solid hsl(${hue}turn 60% 50%)`,
+        // borderTop: "none",
+        display: "inline-block",
+        width: "auto",
+      }}
+    >
+      {index + 1 < list.length && (
+        <HumanCosmology list={list} index={index + 1} height={height} />
+      )}
       <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-        }}
+        style={
+          index + 1 < list.length
+            ? {
+                height: `20px`,
+                width: `200px`,
+              }
+            : {
+                width: `200px`,
+              }
+        }
       >
-        <HumanCosmology list={hcList} height={800} />
+        &nbsp;
       </div>
     </div>
   )
@@ -61,40 +108,6 @@ const DimensionComplexity: FC<{
       <div
         style={{
           height: `${height}px`,
-        }}
-      ></div>
-    </div>
-  )
-}
-
-const HumanCosmology: FC<{
-  list: unknown[]
-  index?: number
-  height: number
-}> = ({ list, index = 0, height }) => {
-  // const self = list.at(-1)
-  const radius = 60 + 20 * (list.length - index - 1)
-  const hue = 0.6 * (index / (list.length - 1))
-  return (
-    <div
-      style={{
-        padding: "0 30px",
-        borderBottomLeftRadius: radius,
-        borderBottomRightRadius: radius,
-        background: `hsl(${hue}turn 60% 70%)`,
-        border: `5px solid hsl(${hue}turn 60% 50%)`,
-        borderTop: "none",
-        display: "inline-block",
-        width: "auto",
-      }}
-    >
-      {index + 1 < list.length && (
-        <HumanCosmology list={list} index={index + 1} height={height} />
-      )}
-      <div
-        style={{
-          height: `${height / list.length}px`,
-          width: `${(height / list.length) * 1.5}px`,
         }}
       ></div>
     </div>
