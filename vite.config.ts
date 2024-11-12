ae57507c2c620f339674f35af85c0b703673cfa4
Alex Burner <alex.burner@gmail.com>
2024-11-11 17:50:37 -0800

    switch to github docs outdir

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For github pages
+  // -> https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: "/cosmology-complexity/",
+  build: { outDir: "docs" },
 })
